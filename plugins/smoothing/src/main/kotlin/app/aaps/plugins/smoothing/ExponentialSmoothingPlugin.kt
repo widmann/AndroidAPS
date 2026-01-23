0bc56fd3cbf03455e5d05fec4bf1629998360b51
widmann <widmann@uni-leipzig.de>
2026-01-23 16:52:46 +0100

    Update weights in ExponentialSmoothingPlugin.kt for Libre 1 min data

--- plugins/smoothing/src/main/kotlin/app/aaps/plugins/smoothing/ExponentialSmoothingPlugin.kt
+++ plugins/smoothing/src/main/kotlin/app/aaps/plugins/smoothing/ExponentialSmoothingPlugin.kt
@@ -47,9 +47,9 @@ class ExponentialSmoothingPlugin @Inject constructor(
         //val ssD: ArrayList<Double> = ArrayList() //MP array for deltas of doubly smoothed Blood Glucose
         var windowSize = data.size //MP number of bg readings to include in smoothing window
         val o1_weight = 0.4
-        val o1_a = 0.5
-        val o2_a = 0.4
-        val o2_b = 1.0
+        val o1_a = 0.13
+        val o2_a = 0.13
+        val o2_b = 0.13
         var insufficientSmoothingData = false
 
         // ADJUST SMOOTHING WINDOW TO ONLY INCLUDE VALID READINGS
@@ -63,6 +63,7 @@ class ExponentialSmoothingPlugin @Inject constructor(
             windowSize =
                 (sizeRecords - 1).coerceAtLeast(0) //MP Adjust smoothing window to the size of database if it is smaller than the original window size; -1 to always have at least one older value to compare against as a buffer to prevent app crashes
         }
+        aapsLogger.debug(LTag.GLUCOSE, "sizeRecords $sizeRecords windowSize $windowSize")
 
         //MP: Adjust smoothing window further if a gap in the BG database is detected, e.g. due to sensor errors of sensor swaps, or if 38 mg/dl are reported (xDrip error state)
         for (i in 0 until windowSize) {
@@ -82,10 +83,12 @@ class ExponentialSmoothingPlugin @Inject constructor(
 
         if (windowSize >= 4) { //MP: Require a valid windowSize of at least 4 readings
             o1_sBG.add(data[windowSize - 1].value) //MP: Initialise smoothing with the oldest valid data point
+            aapsLogger.debug(LTag.GLUCOSE, "data[windowSize - 1].value: " + " o1_sBG[0]: " + o1_sBG[0])
             for (i in 0 until windowSize) { //MP calculate smoothed bg window of valid readings
                 o1_sBG.add(
                     0,
                     o1_sBG[0] + o1_a * (data[windowSize - 1 - i].value - o1_sBG[0])
+                    aapsLogger.debug(LTag.GLUCOSE, "i: " + i + " data[windowSize - 1 - i].value: " + " o1_sBG[0]: " + o1_sBG[0])
                 ) //MP build array of 1st order smoothed bgs
             }
         } else {
@@ -118,6 +121,7 @@ class ExponentialSmoothingPlugin @Inject constructor(
             for (i in o2_sBG.indices) { //MP calculated doubly smoothed bg of all o1/o2 smoothed data available; o2 & o1 smooth bg array sizes are equal in size, so only one is used as a condition
                 // here
                 ssBG.add(o1_weight * o1_sBG[i] + (1 - o1_weight) * o2_sBG[i]) //MP build array of doubly smoothed bgs
+                aapsLogger.debug(LTag.GLUCOSE, "i: " + i + " o1_sBG: " + o1_sBG[i] + " o2_sBG: " + o2_sBG[i])
             }
             for (i in 0 until minOf(ssBG.size, data.size)) { // noise at the beginning of the smoothing window is the greatest, so only include the 10 most recent values in the output
                 data[i].smoothed = max(round(ssBG[i]), 39.0) //Make 39 the smallest value as smaller values trigger errors (xDrip error state = 38)
